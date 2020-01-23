bd2691eac7e0962d772bd18e1294bd65adc5d6cf
Jiří Pihrt <jira.pihrt@volny.cz>
2020-01-23 14:10:35 +0100

    forgot to move this

--- ChoreographyHive/choreography/utils.py
+++ ChoreographyHive/choreography/utils.py
@@ -1,7 +1,7 @@
-from math import floor
+import math
 from typing import List
 
-from rlutilities.linear_algebra import vec3, norm, normalize, clip
+from rlutilities.linear_algebra import vec3, norm, normalize, clip, dot
 
 
 def distance(position: vec3, target: vec3) -> float:
@@ -24,19 +24,22 @@ class CubicBezierCurve:
     def position(self, t: float) -> vec3:
         t = clip(t, 0, 1)
         s = 1 - t
-
-        # if len(self.points) == 2:
-        #     return s * self.points[0] + t * self.points[1]
-        #
-        # if len(self.points) == 3:
-        #     return s * s * self.points[0] + 2 * s * t * self.points[1] + t * t * self.points[2]
-
         return s * s * s * self.p0 + 3 * s * s * t * self.p1 + 3 * s * t * t * self.p2 + t * t * t * self.p3
 
     def tangent(self, t: float) -> vec3:
         t = clip(t, 0, 1)
         s = 1.0 - t
         return 3 * s * s * (self.p1 - self.p0) + 6 * s * t * (self.p2 - self.p1) + 3 * t * t * (self.p3 - self.p2)
+
+    def t_nearest(self, pos: vec3) -> float:
+        t = 0.5
+        for i in range(6):
+            r = self.position(t) - pos
+            dr = self.tangent(t)
+            if abs(dot(r, normalize(dr))) < 10.0:
+                break
+            t -= dot(r, dr) / dot(dr, dr)
+        return t
 
     def to_points(self, n: int) -> List[vec3]:
         return [self.position(i / n) for i in range(n)]
@@ -58,8 +61,8 @@ class BezierPath:
 
     def position(self, t: float) -> vec3:
         n = len(self.curves)
-        i = clip(floor(t * n), 0, n - 1)
-        return self.curves[i].position(t % (1 / n) * n)
+        i = clip(math.floor(t * n), 0, n - 1)
+        return self.curves[i].position((t - i / n) * n)
 
     def to_points(self, n: int) -> List[vec3]:
         return [self.position(i / n) for i in range(n)]
