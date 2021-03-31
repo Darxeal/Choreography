3836926b02c98caf485ab1f562c00fadcdbe4a38
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-03-31 21:00:19 +0200

    Finish flag

--- ChoreographyHive/choreography/choreos/warp.py
+++ ChoreographyHive/choreography/choreos/warp.py
@@ -1,7 +1,10 @@
 from math import sin, cos, pi, sqrt
+
+from rlbot.utils.game_state_util import GameState
+from rlutilities.mechanics import AerialTurn
 from typing import List
 
-from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.utils.structures.game_data_struct import FieldInfoPacket, GameTickPacket
 from rlbot.utils.structures.game_interface import GameInterface
 
 from choreography.choreography import Choreography
@@ -17,18 +20,18 @@ class CheckerFlag(Choreography):
 
     @staticmethod
     def get_num_bots():
-        return 64
+        return 63
 
     @staticmethod
     def get_appearances(num_bots: int) -> List[str]:
-        appearances = ["flag.cfg"] * 64
+        appearances = ["flag.cfg"] * num_bots
         return appearances
 
     @staticmethod
     def get_teams(num_bots: int) -> List[int]:
-        line = [0, 1] * 4
-        teams = (line + line[::-1]) * 4
-        return teams
+        line = [0, 1] * 10 + [0]
+        invert = [0 if x else 1 for x in line]
+        return line + invert + line
 
     @staticmethod
     def get_names(num_bots: int) -> List[str]:
@@ -43,10 +46,11 @@ class CheckerFlag(Choreography):
         ]
 
 
-CENTRE = vec3(0, 0, 5000)
-DIRECTION = vec3(1, 0, 0)
-SPACING = 200
-ROW_SIZE = 8
+CENTRE = vec3(-16500, 29000, 5750)
+DIRECTION = vec3(0, 1, 0)
+H_SPACING = 175
+V_SPACING = 175
+ROW_SIZE = 21
 
 class FlagSetup(TwoTickStateSetStep):
     def set_drone_states(self, drones: List[Drone]):
@@ -55,25 +59,29 @@ class FlagSetup(TwoTickStateSetStep):
         for index, drone in enumerate(drones):
             x = index % ROW_SIZE
             y = index // ROW_SIZE
-            drone.position = CENTRE + (hor * x + ver * y) * SPACING
+            drone.position = CENTRE + (hor * x * H_SPACING) + (ver * y * V_SPACING)
             drone.orientation = look_at(vec3(0, 0, 1), cross(hor, vec3(0, 0, 1)))
             drone.velocity = vec3(0, 0, 0)
             drone.angular_velocity = vec3(0, 0, 0)
 
+    def set_ball_state(self, ball):
+        ball.position = vec3(7400, 30150, -1000) # vec3(7400, 30150, 6600)
+        ball.velocity = vec3(0, 0, 0)
+
 class Flag(DroneListStep):
-    duration = 20.0
+    duration = 60.0
     def step(self, packet: GameTickPacket, drones: List[Drone]):
         hor = normalize(DIRECTION)
         ver = vec3(0, 0, 1)
         perp = cross(hor, ver)
 
-        start = CENTRE + hor * -200 * self.time_since_start
+        start = CENTRE # + hor * -250 * self.time_since_start
 
         for index, drone in enumerate(drones):
             x = index % ROW_SIZE
             y = index // ROW_SIZE
-            target = start + (hor * x + ver * y) * SPACING
-            target += perp * sin(2 * (self.time_since_start - (x + y)/6)) * 300
+            target = start + (hor * x * H_SPACING) + (ver * y * V_SPACING)
+            target += perp * sin(2 * (self.time_since_start - (x + y)/6)) * 700
             drone.hover.target = target
             drone.hover.up = dot(perp, axis_to_rotation(vec3(0, 0, 1) * max(3 * self.time_since_start - (x + y)/3, 1E-9)))
             drone.hover.step(self.dt)
@@ -81,7 +89,8 @@ class Flag(DroneListStep):
 
 
 class Explosion(Choreography):
-    map_name = "AquaDome"
+    map_name = "UtopiaRetro"
+    # map_name = "AquaDome"
 
     @staticmethod
     def get_num_bots():
@@ -103,27 +112,25 @@ class Explosion(Choreography):
 
     def generate_sequence(self):
         self.sequence = [
-            ExplosionPrep(vec3(-10000, 0, 10000), 0),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 1),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 2),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 3),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 4),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 5),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 6),
-            Boom(),
-            ExplosionPrep(vec3(-10000, 0, 10000), 20),
-            Boom(),
+            # ExplosionPrep(vec3(10_000, 0, 5_000), 21),
+            # Boom(vec3(10_000, 0, 5_000)),
+            Test(),
+            Wait(1),
         ]
+
+class Test(StateSettingStep):
+    def perform(self, *args):
+        interface = next((arg for arg in args if isinstance(arg, GameInterface)), None)
+        if interface:
+            field_info = FieldInfoPacket()
+            interface.update_field_info_packet(field_info)
+            print([g.location for g in field_info.goals[:field_info.num_goals]])
+        return super().perform(*args)
 
 
 class ExplosionPrep(TwoTickStateSetStep):
-    radius = 1000
+    duration = 1.0
+    radius = 200
     def __init__(self, position: vec3, freq: float = 20):
         super().__init__()
         self.position = position
@@ -144,13 +151,20 @@ class ExplosionPrep(TwoTickStateSetStep):
             drone.velocity = dir * 2300
             drone.angular_velocity = vec3(10, 20, 30)
 
-from random import random
+class Boom(PerDroneStep):
+    duration = 3.0
 
-class Boom(BlindBehaviorStep):
-    duration = 0.5
-    def set_controls(self, controls):
-        controls.boost = True
-        # controls.yaw = 2 * random() - 1
-        # controls.pitch = 2 * random() - 1
-        # controls.roll = 2 * random() - 1
+    def __init__(self, position: vec3, freq: float = 20):
+        super().__init__()
+        self.position = position
 
+    def step(self, packet: GameTickPacket, drone: Drone, index: int):
+        dir = normalize(drone.position - self.position)
+        drone.aerial_turn.target = look_at(dir, vec3(0, 0, 1))
+        drone.aerial_turn.step(self.dt)
+        drone.controls = drone.aerial_turn.controls
+        # drone.controls.yaw = 1.0
+        # drone.controls.pitch = 1.0
+        # drone.controls.roll = -1.0
+        drone.controls.boost = True
+        
